59ff2bd961afbad82208f30c19a1f0a73377da13
Tejas Jiddewar <tejasjiddewar955@gmail.com>
2025-06-13 21:44:01 +0530

    total revenue from pizza sales

--- project.session.sql
+++ project.session.sql
@@ -12,6 +12,11 @@ MODIFY COLUMN date DATE;
 --Retrieve the total number of orders placed.
 select count(order_id) from orders;
 
+--Calculate the total revenue generated from pizza sales.
+SELECT sum(order_details.quantity * pizzas.price) 
+FROM order_details
+JOIN pizzas
+ON order_details.pizza_id=pizzas.pizza_id;
 
 
 

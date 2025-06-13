77e3d27ce2fe9a4d5b1296993066077d98960159
Tejas Jiddewar <tejasjiddewar955@gmail.com>
2025-06-13 17:42:34 +0530

    retrieved details

---
+++ project.session.sql
@@ -0,0 +1,16 @@
+USE pizza_sales;
+SHOW tables;
+
+SELECT * FROM order_details;
+SELECT * FROM orders;
+SELECT * FROM pizza_types;
+SELECT * FROM pizzas;
+
+ALTER TABLE orders
+MODIFY COLUMN date DATE;
+
+
+
+
+
+
